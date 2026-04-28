af37489e0529166af4c1a79cccb878cdd5c5fe36
SebastianAM08 <thewindislikelife@gmail.com>
2026-04-28 09:08:15 -0500

    Estructura inicial de React y configuración de Axios

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react'
+import clienteAxios from './api/axios'
+
+function App() {
+  const [mensaje, setMensaje] = useState('Conectando...')
+
+  useEffect(() => {
+    // Intentamos llamar a una ruta de prueba en Laravel
+    clienteAxios.get('/test') 
+      .then(res => setMensaje(res.data.message))
+      .catch(err => setMensaje('Error: Revisa el CORS en Laravel'))
+  }, [])
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Proyecto Laravel_ATHENA</h1>
+      <p>Estado del Backend: <strong>{mensaje}</strong></p>
+    </div>
+  )
+}
+
+export default App
